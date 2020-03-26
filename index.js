f5d5282912fe07a770243b195c32f488b158c223
iwillitried <waknoepp@hotmail.com>
2020-03-26 23:01:16 +0100

    Added gapi stuff (hstill bugged!)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const nameField = document.getElementById("albumTitelInput");
 const artistField = document.getElementById("artistField");
 const priceField = document.getElementById("priceField");
@@ -5,10 +6,48 @@ const durationField = document.getElementById("durationField");
 const tracksField = document.getElementById("tracksField");
 const cdsField = document.getElementById("cdsField");
 
-const submitButton = document.getElementById("submit")
-submitButton.addEventListener("submit", submitClicked);
+const submitButton = document.getElementById("submit") // Listener to submitClicked() added in html
+
 
 function submitClicked() {
-  console.log("Album Name: " + nameField.value);
+  var data = {
+    "album" : {
+      "meta" : {
+        "name" : nameField.value,
+        "artist" : artistField.value,
+        "album_length" : durationField.value,
+        "album_songs" : tracksField.value,
+        "album_cds" : cdsField.value,
+        "price" : priceField.value
+      },
+      "songs" : {
+        "song" : []
+      }
+    }
+  }
+
+  // googleAuth.signIn();
+  // initClient();
+
+  // var headers = {
+  //   "Authorization" : "Bearer ["+ACCESS_TOKEN+"]",
+  //   "Accept" : "application/json",
+  //   "Content-Type" : "application/json"
+  // }
+  //
+  // var options = {
+  //   "mimeType": "application/vnd.google-apps.folder",
+  //   "name": "New Folder!"
+  // }
+  //
+  // console.log("headers: \n"+headers);
+  // fetch("https://www.googleapis.com/drive/v3/files?key=["+GAPI_KEY+"]", {
+  //   method : "post",
+  //   headers : headers,
+  //   body : JSON.stringify(options)
+  // }).then( res => console.log(res))
+  // .catch( err => console.log(err));
+
+  // console.log(data);
 
 }
