4cd60e3cc571156db77238bedfcea8a7045b0a86
iwillitried <waknoepp@hotmail.com>
2020-03-29 19:44:33 +0200

    Pick Artwork and MP3 Files

--- gapi.js
+++ gapi.js
@@ -110,23 +110,62 @@ function appendPre(message) {
 
 // Upload Test File
 function createFolder(name) {
-  console.log("Creating new folder: "+name);
-  metadata = {
-    "name" : name,
-    "mimeType" : "application/vnd.google-apps.folder"
-  }
-  gapi.client.drive.files.create({
-    resource: metadata,
-    fields: "id"
-  }, function(err, data) {
-    if (err) {
-      console.log("Failed! "+err);
-      appendPre(err);
-    } else {
-      console.log("Success! "+data);
-      appendPre("Created folder '"+name+"', ID:"+data.id);
-    }
+  return new Promise(function(resolve, reject) {
+    console.log("Creating new folder: "+name);
+
+    var parentId = '';//some parentId of a folder under which to create the new folder
+    var fileMetadata = {
+      'name' : name,
+      'mimeType' : 'application/vnd.google-apps.folder'
+    };
+    gapi.client.drive.files.create({
+      resource: fileMetadata,
+    }).then(function(response) {
+      switch(response.status){
+        case 200:
+          var file = response.result;
+          resolve(file.id);
+          break;
+        default:
+          reject(response);
+          break;
+        }
+    });
+    console.log("bye!");
+  });
+}
+
+function addAlbumDataToFolderWithID(id, data) {
+  return new Promise(function(resolve, reject) {
+
+    var fileContent = data; // As a sample, upload a text file.
+    var file = new Blob([fileContent], {type: 'text/plain'});
+    var metadata = {
+        'name': 'info.txt', // Filename at Google Drive
+        'mimeType': 'text/plain', // mimeType at Google Drive
+        'parents': [id], // Folder ID at Google Drive
+    };
+
+    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+    var form = new FormData();
+    form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+    form.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        switch (xhr.response.status) {
+          case 200:
+            resolve(xhr.response.data.id);
+            break;
+          default:
+            reject(xhr.response);
+        }
+        resolve();
+    };
+    xhr.send(form);
 
   });
-  console.log("bye!");
 }
--- index.js
+++ index.js
@@ -6,14 +6,78 @@ const durationField = document.getElementById("durationField");
 const tracksField = document.getElementById("tracksField");
 const cdsField = document.getElementById("cdsField");
 
-const submitButton = document.getElementById("submit") // Listener to submitClicked() added in html
+const submitButton = document.getElementById("submit"); // Listener to submitClicked() added in html
+const trackArray = [];
+const artworkFile = ""; 
 
+function removeTrackField(e) {
+  let trackNumber = e.target.id.charAt(0);
+  let trackbox = document.getElementById("trackbox");
+  let trackField = document.getElementById(""+trackNumber+"trackField");
+  trackbox.removeChild(trackField);
+  // Remove file from array
+  trackArray.splice(trackNumber, 1);
+
+
+}
+
+function handleTrackFiles(files) {
+  let file = files[0];
+  let audioType = /^audio\//;
+
+  if (!audioType.test(file.type)) {
+    return;
+  }
+
+  // Build a TrackField
+  let trackbox = document.getElementById("trackbox");
+  let trackField = document.createElement("div");
+  let trackFieldP = document.createElement("P");
+  let trackFieldButton = document.createElement("BUTTON");
+  // Fill in Button and Label properties
+  trackFieldP.innerText = file.name;
+  trackFieldP.style = "width: 50%;float: left;margin-top: 20px;margin-bottom: 0px;margin-left: 10px;font-size: 18px;";
+  trackFieldButton.innerText = "X";
+  trackFieldButton.id = trackArray.length + "trackFieldButton";
+  trackFieldButton.addEventListener("click", removeTrackField);
+  trackFieldButton.style = "width: 20%;text-align: center;float: right;margin-top: 10px;margin-bottom: 0px;margin-right: 10px;font-size: 20px;";
+  // Fill in  TrackField properties
+  trackField.class = "trackField";
+  trackField.id = "" + trackArray.length + "trackField";
+  trackField.style = "height: 60px;border-bottom: 1px solid grey;border: 1px solid grey;";
+
+  // Add everything to the page and the file in the array
+  trackField.appendChild(trackFieldP);
+  trackField.appendChild(trackFieldButton);
+  trackbox.appendChild(trackField);
+  trackArray.push(file);
+
+}
+
+function handleArtworkFiles(files) {
+  let file = files[0];
+  let imageType = /^image\//;
+  // see if its an image file
+  if (!imageType.test(file.type)) {
+    return;
+  }
+  // replace the current artwork image with the new one
+  let reader = new FileReader();
+  reader.onload = function() {
+    let artworkImage = document.getElementById("artworkImage");
+    let parent = artworkImage.parentNode;
+    let newArtworkImage = artworkImage;
+    newArtworkImage.src = reader.result;
+    artworkImageParent.replaceChild(newArtworkImage, artworkImage);
+  }
+  reader.readAsDataURL(file);
+}
 
 function submitClicked() {
   // If Button is disabled, ignore event
   if (submitButton.className == "disabled") return;
 
-  createFolder(nameField.value); 
+
   var data = {
     "album" : {
       "meta" : {
@@ -29,6 +93,11 @@ function submitClicked() {
       }
     }
   }
+
+  createFolder(nameField.value)
+  .then(id => addAlbumDataToFolderWithID(id, JSON.stringify(data)))
+  .then(/* do nothing */)
+  .catch(err => console.log("Error creating folder: \n"+ err));
 
 
 
